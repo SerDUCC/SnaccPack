51b123e21bfdfacd8e94f06eb1d364984ec1f7b2
Deadly <info.saddiki@gmail.com>
2021-09-23 14:44:48 +0200

    More consistency
    More consistency across the board, renamed the id, to better fit.

--- README.txt
+++ README.txt
@@ -2,3 +2,6 @@ SNACC Pack™
 
 	The SNACC Pack™ is a lightweight, easy to use
 	portable pouch for all your SNACCing needs!
+
+	Enable it through config/config.json.
+	You're also able to change it's storage grid.
--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 /*
 HONK
 */
- 
+
 "use strict";
 
 const conf = require("../config/config.json")
@@ -10,37 +10,37 @@ const modPath = ModLoader.getModPath("ducc-SnaccPack");
 class SnaccPack {
 	constructor() {
 		this.mod = "ducc-SnaccPack";
-		Logger.info("Loading: My ass");
+		Logger.info("Loading: SnaccPack");
 		ModLoader.onLoad[this.mod] = this.load.bind(this);
 	}
- 
+
 	load() {
 		if (conf.snaccPack["enabled"]) {
 
 			// Items are added
-			const item_path = "db/items/honk.json";
+			const item_path = "db/items/snaccPack.json";
 			const item_object = JsonUtil.deserialize(VFS.readFile(`${modPath}/${item_path}`));
 			DatabaseServer.tables.templates.items[item_object._id] = item_object;
 
 			// Handbook entries are added
-			const handbook_path = "db/templates/handbook/honk.json";
+			const handbook_path = "db/templates/handbook/snaccPack.json";
 			const handbook_item = JsonUtil.deserialize(VFS.readFile(`${modPath}/${handbook_path}`));
 			DatabaseServer.tables.templates.handbook.Items.push(handbook_item)
 
 			// Locale is fixed
-			const locale_path = "db/locales/en/templates/honk.json";
-			DatabaseServer.tables.locales.global.en.templates["honk"] = JsonUtil.deserialize(VFS.readFile(`${modPath}/${locale_path}`));
+			const locale_path = "db/locales/en/templates/snaccPack.json";
+			DatabaseServer.tables.locales.global.en.templates["snaccPack"] = JsonUtil.deserialize(VFS.readFile(`${modPath}/${locale_path}`));
 
 			// Add item to traders
-			const ragfair_path = "db/assort/ragfair/items/honk.json";
+			const ragfair_path = "db/assort/ragfair/items/snaccPack.json";
 			const ragfair_item = JsonUtil.deserialize(VFS.readFile(`${modPath}/${ragfair_path}`));
 			DatabaseServer.tables.traders.ragfair.assort.items.push(ragfair_item);
 
-			const loyal_path = "db/assort/ragfair/loyal_level_items/honk.json";
-			DatabaseServer.tables.traders.ragfair.assort.loyal_level_items["honk"] = JsonUtil.deserialize(VFS.readFile(`${modPath}/${loyal_path}`));
+			const loyal_path = "db/assort/ragfair/loyal_level_items/snaccPack.json";
+			DatabaseServer.tables.traders.ragfair.assort.loyal_level_items["snaccPack"] = JsonUtil.deserialize(VFS.readFile(`${modPath}/${loyal_path}`));
 
-			const barter_path = "db/assort/ragfair/barter_scheme/honk.json";
-			DatabaseServer.tables.traders.ragfair.assort.barter_scheme["honk"] = JsonUtil.deserialize(VFS.readFile(`${modPath}/${barter_path}`));
+			const barter_path = "db/assort/ragfair/barter_scheme/snaccPack.json";
+			DatabaseServer.tables.traders.ragfair.assort.barter_scheme["snaccPack"] = JsonUtil.deserialize(VFS.readFile(`${modPath}/${barter_path}`));
 
 
 			DatabaseServer.tables.templates.items["honk"]._props.Grids[0]._props.cellsH = conf.snaccPack["internalHeight"];
